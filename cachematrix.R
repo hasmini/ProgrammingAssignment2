4ed0e1bfa5ef4c6da0a371114f96c5de27a166af
hasmini <fauzihasmini@tm.com.my>
2015-06-22 06:26:21 +0800

    update
    lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  #set matrix inverse
+  ##set matrix inverse
   inverse <- NULL
   
   ##create a function
